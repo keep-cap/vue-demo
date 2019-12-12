cd4da510ab0864ec2b1b4cd1bcba46610b85d86f
keep-cap <675706875@qq.com>
2019-12-12 19:03:39 +0800

    Vue-cli lifeCircle

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <LifeCircle />
+  </div>
+</template>
+
+<script>
+import LifeCircle from './components/LifeCircle.vue'
+
+export default {
+  name: 'app',
+  components: {
+    LifeCircle
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/LifeCircle.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <p v-if="isShow">{{ str1 }}</p>
+    <p v-else>{{ str2 }}</p>
+    <button @click="destory">销毁</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"LifeCircle",
+    beforeCreate() {
+      console.log('1:beforeCreate()');
+    },
+    data() {
+        return {
+          isShow:false,
+          str1:'项目',
+          str2:'demo'
+        }
+    },
+    methods: {
+      destory() {
+        this.$destroy()
+      }
+    },
+    created() {
+      console.log('2:created()');
+    },
+    beforeMount() {
+      console.log('3:beforeMount()');
+    },
+    mounted() {
+      console.log('4:mounted()');
+      //定时器
+      this.intervalId = setInterval(()=>{
+        console.log('+-----------+')
+        this.isShow = !this.isShow;
+      },1000)
+    },
+    beforeUpdate() {
+      console.log('5:beforeUpdate()');
+    },
+    updated() {
+      console.log('6:updated()');
+    },
+    beforeDestroy() {
+      console.log('7:beforeDestroy()');
+      //清楚定时器
+      clearInterval(this.intervalId)
+    },
+    destroyed() {
+      console.log('8:destroyed()');
+    }
+}
+</script>
+
+<style>
+
+</style>
